a71132ada70579df35fad9dd07a50c0a11da44d8
Louis Savoie <thelousavoie@gmail.com>
2020-09-09 23:29:15 -0400

    install dotenv

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+require('dotenv').config();
 
 // CONNECT MONGOOSE TO MONGODB
 mongoose.connect(process.env.DATABASEURL, {
