ca304072ad7235c678b461d507ee3ffc278b7770
Louis Savoie <thelousavoie@gmail.com>
2021-01-24 12:01:01 -0500

    header.ejs rewrite, set up head with SEO, favicons, bootstrap5, fontawesome, google fonts, remove old header

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -3,15 +3,15 @@ const burger = document.querySelector('.burger i');
 const nav = document.querySelector('nav');
 const header = document.querySelector('header');
 
-// Defining a function
+// Toggle Function
 function toggleNav() {
     burger.classList.toggle('fa-bars');
     burger.classList.toggle('fa-times');
     nav.classList.toggle('nav-active');
     header.classList.toggle('header-active');
-}
+};
 
-// Calling the function after click event occurs
+// Toggling upon click event
 burger.addEventListener('click', function() {
     toggleNav();
 });
