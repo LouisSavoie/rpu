7b5a2d8d0880d98f2fdb57fbf54d824902d44596
Louis Savoie <thelousavoie@gmail.com>
2021-01-30 16:12:55 -0500

    add login and signup GET routes, create login and signup pages

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get("/", (req, res) => {
     res.render("landing");
 });
 
+// GET Sign Up Form
+router.get("/signup", (req, res) => {
+    res.render("signup");
+});
+
+// GET Log In Form
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
 module.exports = router;
