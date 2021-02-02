488c8a70f0a3fccac25324c5dc4cb42d34e74e3b
Louis Savoie <thelousavoie@gmail.com>
2021-02-01 19:43:41 -0500

    add sign up page content and style

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ passport.deserializeUser(User.deserializeUser());
 
 app.use(function(req, res, next){
     res.locals.currentUser = req.user;
-    res.locals.error = req.flash("error");
-    res.locals.success = req.flash("success");
+    // res.locals.error = req.flash("error");
+    // res.locals.success = req.flash("success");
     next();
 });
 
