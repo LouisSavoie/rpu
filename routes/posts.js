273f2231864d337d767d8e328cd3fde6923c5ad7
Louis Savoie <thelousavoie@gmail.com>
2021-03-09 15:32:20 -0500

    add posts array to character mongoose model, add new post POST route

--- routes/posts.js
+++ routes/posts.js
@@ -20,4 +20,34 @@ router.get("/characters/:id/posts/new", middleware.checkCharacterOwnership, func
     });
 });
 
+// POST New Post Form
+router.post("/characters/:id/posts", middleware.checkCharacterOwnership, function(req, res){
+    // find Character by id
+    Character.findById(req.params.id, function(err, character){
+        if(err){
+            console.log(err);
+            res.redirect("/characters/" + character._id);
+        } else {
+          // create new Post
+          Post.create(req.body.post, function(err, post){
+            if(err){
+                console.log(err);
+            } else {
+                // add user id to Post
+                post.user = req.user._id;
+                post.skill = req.body.post.skill;
+                post.text = req.body.post.text;
+                post.image = req.body.post.image;
+                post.save();
+                // connect new Post to Character
+                character.posts.push(post);
+                character.save();
+                // redirect to chracter show page
+                res.redirect("/characters/" + character._id);
+            }
+          });
+        }
+    });
+});
+
 module.exports = router;
